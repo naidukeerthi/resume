d563cc792910082deca3221ddda4f5d80551e401
naidukeerthi <120628583+naidukeerthi@users.noreply.github.com>
2023-01-02 10:32:53 +0530

    Add files via upload

---
+++ script11.js
@@ -0,0 +1,52 @@
+var head1=[ 
+    {
+        
+           careerobjective : "to be a part o fo company that indulges professional growth which provides challenging and rewaarding career whilw allowing me to utilize my knowledge and skills.",
+    },
+    {
+    summary :"i am good fun and friendly person, i am honest and punctual. i ahve good communication skill. i have creative mind and am always realy for new challenges. i can deal with difficulti situations. maybe a lot of people are more the good then me, but with perseverance and strong will can defeat all."
+    },
+    {
+        personalskills:"vreativity, interpersonal skills, critical thinking, problem solving, public speaking, customer service skills, teamwork skill, communication."
+    }
+    ]
+     var second =[
+    {
+        Qualification:'Bsc., Physics',
+        Schoolcollege:'ARCOT SRI MHALAKSHMI WOMENS COLLEGE',
+        Board:'Thiruvallvar university',
+        yearofpassing:'2021',
+        percentageCGPA:'78%'
+    },
+    {
+        Qualification:'HSC',
+        Schoolcollege:'GOVT GIRLS HIGHER SECONDARY SCHOOL,GURUVARAJAPET',
+        Board:'State board',
+        yearofpassing:'2018',
+        percentageCGPA:'63%'
+    },
+    {
+        Qualification:'SSLC',
+        Schoolcollege:'GOVT GIRLS HIGHER SECONDORY SCHOOL,GURUVARAJAPET',
+        Board:'State board',
+        yearofpassing:'2016',
+        percentageCGPA:'73.6%'
+    }
+     ]
+    
+     var skills=
+        ["c","c++","java","HTML5", "CSS3", "JAVASCRIPT", "RDBMS,", "SQL", "ms office"]
+        var personalinfo={
+    name:"V,Keerthana",
+    Father:"C.Vengadesan",
+    DOB:"07.06.2001",
+    Gender:"Female",
+    Materialstatus:"single",
+    Nationality:"indian",
+    Religion:"hindu",
+    Mobileno: 9150820842
+        }
+    console.log(head1)
+    console.table(second)
+    console.log(skills)
+    console.table(personalinfo)
